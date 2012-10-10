7e3578378ebe94db55bb84a26b2d1dc5e962f8d7
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-10 15:14:00 +0200

    Adding function_exists for the functions

--- application/helpers.php
+++ application/helpers.php
@@ -3,24 +3,27 @@
  * Part of Fonto framework
  */
 
-function _vd($dumpy)
-{
-	echo "<pre>";
-	var_dump($dumpy);
-	echo "</pre>";
-	die;
+if ( ! function_exists('_vd')) {
+	function _vd($data) {
+		echo "<pre>";
+		var_dump($data);
+		echo "</pre>";
+		die;
+	}
 }
 
-function _pr($dumpy)
-{
-	echo "<pre>";
-	print_r($dumpy);
-	echo "</pre>";
-	die;
+if (!function_exists('_pr')) {
+	function _pr($data) {
+		echo "<pre>";
+		print_r($data);
+		echo "</pre>";
+		die;
+	}
 }
 
-function _ed($dumpy)
-{
-	echo $dumpy;
-	die;
+if (!function_exists('_ed')) {
+	function _ed($data) {
+		echo $data;
+		die;
+	}
 }
