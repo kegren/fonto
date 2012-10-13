6bb03f71c42b14a94002a00384ea18e3471ffc40
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-13 02:42:46 +0200

    Adding controller path

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ defined('EXT') or define('EXT', '.php');
 defined('ROOT') or define('ROOT', realpath(__DIR__). DS);
 defined('APPPATH') or define('APPPATH', ROOT . 'application' . DS);
 defined('APPWEBPATH') or define('APPWEBPATH', APPPATH . 'src' . DS . 'Web' . DS);
+defined('CONTROLLERPATH') or define('CONTROLLERPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Controllers' . DS);
 defined('VIEWPATH') or define('VIEWPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Views' . DS);
 defined('VENDORPATH') or define('VENDORPATH', ROOT . 'vendor' . DS);
 defined('SYSCOREPATH') or define('SYSCOREPATH', VENDORPATH . 'fonto' . DS . 'src' . DS . 'Fonto' . DS . 'Core' . DS);
