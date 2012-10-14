67ce2002fdade46a5d7194c83ed038560e82312a
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-14 18:20:35 +0200

    Add application object and remove unneeded services

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -4,55 +4,17 @@
  * Include files
  */
 include $paths['app'] . 'helpers' . EXT;
-include $paths['core'] . 'Autoloader' . EXT;
-include $paths['core'] . 'FontoException' . EXT;
+include $paths['core'] . 'Application' . EXT;
 
 /**
  * Namespace shortcuts
  */
-use Fonto\Core\Request as Request,
-	Fonto\Core\Router as Router,
-	Fonto\Core\Route as Route,
-	Fonto\Core\Config as Config,
-	Fonto\Core\FontoException as FontoException;
-
+use Fonto\Core\Application as App;
 
 /**
- * Set custom exception handling
+ * Run application
  */
-set_exception_handler(array('\Fonto\Core\FontoException', 'handle'));
-
-
-/**
- * Register autoloading
- */
-$loader = include $paths['vendor'] . 'autoload' . EXT;
-$loader->add('Web', APPPATH . 'src');
-
-
-/**
- * Timezone
- */
-date_default_timezone_set(Config::read('application', 'timezone'));
-
-/**
- * Set/Get all routes
- */
-$routes = include $paths['app'] . 'routes' . EXT;
-
-/**
- * Instantiates objects and sets routes
- */
-$route = new Route();
-$route->addRoutes($routes);
-
-$request = new Request();
-$router = new Router($request, $route);
-$router->route();
-
-/**
- * $app = new Application();
- * $app->run();
- */
+$app = new App();
+$app->run();
 
 unset($paths);
