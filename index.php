695fe8aa5b15e953dc2927e72454066c32217e02
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-18 17:14:37 +0200

    Add modelpath

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Part of Fonto Framework
+ *
+ * Define constants and set paths
+ */
+
 
 /**
  * Define custom constants
@@ -15,6 +21,7 @@ defined('APPWEBPATH') or define('APPWEBPATH', APPPATH . 'src' . DS . 'Web' . DS)
 defined('CONFIGPATH') or define('CONFIGPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Config' . DS);
 defined('CONTROLLERPATH') or define('CONTROLLERPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Controllers' . DS);
 defined('VIEWPATH') or define('VIEWPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Views' . DS);
+defined('MODELPATH') or define('MODELPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Models' . DS);
 defined('VENDORPATH') or define('VENDORPATH', ROOT . 'vendor' . DS);
 defined('FONTOCOREPATH') or define('FONTOCOREPATH', VENDORPATH . 'fonto' . DS . 'fonto' . DS . 'src' . DS . 'Fonto' . DS . 'Core' . DS);
 defined('WEBPATH') or define('WEBPATH', ROOT . 'web' . DS);
