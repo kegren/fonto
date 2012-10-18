03110735a1fb9b0247820c847e9635873cc6e025
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-18 18:13:29 +0200

    Remove all methods

--- vendor/fonto/fonto/src/Fonto/Core/Model.php
+++ vendor/fonto/fonto/src/Fonto/Core/Model.php
@@ -1,8 +1,13 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/Fonto
+ */
 
 namespace Fonto\Core;
 
-class Model extends ActiveRecord\Model
-{
-
-}
+class Model extends \ActiveRecord\Model
+{}
