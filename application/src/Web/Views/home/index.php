f9b973802aa1e4768cf64f985d7ffd4bc9e565d0
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-18 09:04:48 +0200

    Adding html5 shim and twig

--- application/src/Web/Views/home/index.php
+++ application/src/Web/Views/home/index.php
@@ -3,11 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Fonto</title>
+	<link rel="stylesheet" href="">
+
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
 <body>
-	<div class="home-container">
-		<h1>Controller: Home!</h1>
-		<p> <?php echo $test; ?> </p>
+	<div class="fontoContainer">
+		<header>
+			<h1>{{ title }}</h1>
+		</header>
+		<section>
+			<p>{{ text }}</p>
+		</section>
 	</div>
 </body>
 </html>
