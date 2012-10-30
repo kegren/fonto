3f8abad769fcaff7906aa455cb87bc7443f47a07
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-30 11:08:57 +0100

    Update paths and namespace for main app

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,12 +10,12 @@
  * Include files
  */
 include APPPATH . 'helpers' . EXT;
-include SYSCOREPATH . 'Application' . EXT;
+include SYSCOREAPPPATH . 'App' . EXT;
 
 /**
  * Namespace alias
  */
-use Fonto\Core\Application as App;
+use Fonto\Core\Application\App as App;
 
 /**
  * Run application
