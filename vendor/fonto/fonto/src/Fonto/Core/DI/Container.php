ef0a248226a204bd52be37394067e83cebbcc466
Kenny Damgren <kenny.damgren@gmail.com>
2012-10-30 22:33:12 +0100

    Added build method

--- vendor/fonto/fonto/src/Fonto/Core/DI/Container.php
+++ vendor/fonto/fonto/src/Fonto/Core/DI/Container.php
@@ -15,27 +15,42 @@ class Container implements ContainerInterface
 {
 	protected $services = array();
 
+	protected $name;
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+	}
+
 	public function set($name, $value)
 	{
 		if (isset($this->services[$name])) {
 			throw new FontoException("There is already an service with $name registered in the container");
 		}
+
 		$this->services[$name] = $value;
 	}
 
 	public function get($name)
 	{
 		if (!isset($this->services[$name])) {
-			throw new FontoException("No service is registrerd with name $name");
+			throw new FontoException("No service is registered with name $name");
 		}
 
-		if ( ! $name instanceOf \Closure) {
-			return $this->services[$name];
+		if (is_callable($this->services[$name])) {
+			return $this->services[$name]($this);
 		} else {
-			return $this->services[$name]();
+			return $this->services[$name];
 		}
 	}
 
-	public function call($class)
-	{}
+	public function build($class)
+	{
+		$args = array_slice(func_get_args(), 1);
+
+		$reflection = new \ReflectionClass($class);
+		$instance   = $args ? $reflection->newInstanceArgs($args) : $reflection->newInstance();
+
+		return $instance;
+	}
 }
