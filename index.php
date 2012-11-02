68eb0c1c9a066f598c86c3f00a4917489c5d8e74
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-02 13:23:01 +0100

    Removed paths to main app

--- index.php
+++ index.php
@@ -17,11 +17,6 @@ defined('EXT') or define('EXT', '.php');
  */
 defined('ROOT') or define('ROOT', realpath(__DIR__). DS);
 defined('APPPATH') or define('APPPATH', ROOT . 'app' . DS);
-defined('APPWEBPATH') or define('APPWEBPATH', APPPATH . 'src' . DS . 'Web' . DS);
-defined('CONFIGPATH') or define('CONFIGPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Config' . DS);
-defined('CONTROLLERPATH') or define('CONTROLLERPATH', APPPATH . 'src' . DS . 'Demo' . DS . 'Controllers' . DS);
-defined('VIEWPATH') or define('VIEWPATH', APPPATH . 'src' . DS . 'Demo' . DS . 'Views' . DS);
-defined('MODELPATH') or define('MODELPATH', APPPATH . 'src' . DS . 'Web' . DS . 'Models' . DS);
 defined('VENDORPATH') or define('VENDORPATH', ROOT . 'vendor' . DS);
 defined('SYSCOREPATH') or define('SYSCOREPATH', VENDORPATH . 'fonto' . DS . 'fonto' . DS . 'src' . DS . 'Fonto' . DS . 'Core' . DS);
 defined('SYSCOREAPPPATH') or define('SYSCOREAPPPATH', VENDORPATH . 'fonto' . DS . 'fonto' . DS . 'src' . DS . 'Fonto' . DS . 'Core' . DS . 'Application' . DS);
