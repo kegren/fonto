829b44cb5b39dd62941dbf121fa140b119249345
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-03 04:03:54 +0100

    Added flashMessage method and class doc

--- vendor/fonto/fonto/src/Fonto/Core/Session.php
+++ vendor/fonto/fonto/src/Fonto/Core/Session.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core;
 
@@ -20,6 +27,17 @@ class Session
 	public function get($id)
 	{
 		return $_SESSION[$id];
+	}
+
+	public function flashMessage($id)
+	{
+		if (isset($_SESSION[$id])) {
+			$message = $_SESSION[$id];
+			unset($_SESSION[$id]);
+			return $message;
+		}
+
+		return '';
 	}
 
 }
