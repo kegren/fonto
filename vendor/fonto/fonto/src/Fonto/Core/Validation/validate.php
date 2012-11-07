693fa618e586ff0fc779f84341a3930a4a6525f3
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-07 19:09:58 +0100

    Added validate method and some validation rules

--- vendor/fonto/fonto/src/Fonto/Core/Validation/validate.php
+++ vendor/fonto/fonto/src/Fonto/Core/Validation/validate.php
@@ -13,6 +13,12 @@ class Validate
 {
 	protected $errors;
 
+	protected $name;
+
+	protected $rules;
+
+	protected $attributes;
+
 	public function __construct()
 	{
 		$this->errors = array();
@@ -23,8 +29,76 @@ class Validate
 		return $this->errors;
 	}
 
+	public function max($max)
+	{
+		$this->rules[$this->name]['isMax'] = $max;
+
+		return $this;
+	}
+
+	public function num($num)
+	{
+		$this->rules[$this->name]['isNumeric'] = true;
+
+		return $this;
+	}
+
+	public function field($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
 	public function isValid()
 	{
 		return empty($this->errors) ? true : false;
 	}
+
+	public function validate($attributes = array())
+	{
+		foreach ($attributes as $id => $value) {
+			$this->attributes[$id] = $value;
+		}
+
+		foreach ($this->attributes as $id => $value) {
+			if ($this->isDefined($id)) {
+				$this->validator($id, $value, $this->rules[$id]);
+				break;
+			}
+		}
+	}
+
+	private function validator($id, $value, $rules)
+	{
+		unset($rules[$id]);
+
+		foreach ($rules as $method => $validateValue) {
+
+			if (method_exists($this, $method)) {
+				call_user_func(array($this, $method), $id, $value, $validateValue);
+			}
+
+		}
+	}
+
+	private function isMax($id, $value, $validateValue)
+	{
+		if (strlen($value) > $validateValue) {
+			$this->errors[$id][0] = 'Värdet är för stort';
+		}
+	}
+
+	private function isDefined($id)
+	{
+		return array_key_exists($id, $this->rules);
+	}
+
+	private function isNumeric($id, $value, $validateValue)
+	{
+		if (!is_numeric($value)) {
+			$this->errors[$id][1] = 'Värdet måste bestå av endast siffror';
+		}
+	}
+
 }
