dedf60af8aa9e860663a8564e3b828e0f570ece0
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-09 07:37:33 +0100

    Updated docs

--- vendor/fonto/fonto/src/Fonto/Core/FormModel/Base.php
+++ vendor/fonto/fonto/src/Fonto/Core/FormModel/Base.php
@@ -1,10 +1,22 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core\FormModel;
 
-use Fonto\Core\Validation\Validate;
+use Fonto\Core\Validation\Validator;
 
 abstract class Base
 {
-	public abstract function rules(Validate $validation);
+	/**
+	 * Rules for the form
+	 *
+	 * @param  Validator $validator
+	 */
+	public abstract function rules(Validator $validator);
 }
