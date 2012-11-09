2ab3ec12e87267014229ac36c46bbb7a21eff577
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-09 11:45:30 +0100

    Changed offset to id and updated docs

--- vendor/fonto/fonto/src/Fonto/Core/DI/Container.php
+++ vendor/fonto/fonto/src/Fonto/Core/DI/Container.php
@@ -12,11 +12,22 @@ namespace Fonto\Core\DI;
 use Fonto\Core\FontoException;
 use Fonto\Core\Application\App;
 use \ArrayAccess;
+use \Closure;
 
 class Container implements ArrayAccess
 {
+	/**
+	 * Services in the container
+	 *
+	 * @var array
+	 */
 	protected $services = array();
 
+	/**
+	 * Fonto\Core\Application\App
+	 *
+	 * @var object
+	 */
 	protected $app;
 
 	public function __construct(App $app)
@@ -24,35 +35,75 @@ class Container implements ArrayAccess
 		$this->app = $app;
 	}
 
-	public function offsetSet($offset, $value)
+	/**
+	 * Registers a service by id
+	 *
+	 * @param  string $id
+	 * @param  string $value
+	 * @return void
+	 */
+	public function offsetSet($id, $value)
 	{
-		if (isset($this->services[$offset])) {
-			throw new FontoException("There is already an service with $offset registered in the container");
+		if (isset($this->services[$id])) {
+			throw new FontoException("There is already an service with $id registered in the container");
 		}
 
-		$this->services[$offset] = $value;
+		$this->services[$id] = $value;
 	}
 
-	public function offsetExists($offset)
+	/**
+	 * Checks if the given service is registered in the container
+	 *
+	 * @param  string $id
+	 * @return boolean
+	 */
+	public function offsetExists($id)
 	{
-		//
+		return isset($this->services[$id]);
 	}
 
-	public function offsetUnset($offset)
+	/**
+	 * Unsets a service
+	 *
+	 * @param  string $id
+	 * @return
+	 */
+	public function offsetUnset($id)
 	{
-		//
+		unset($this->services[$id]);
 	}
 
-	public function offsetGet($offset)
+	/**
+	 * Returns the registered service if exists
+	 *
+	 * @param  string $id
+	 * @return mixed
+	 */
+	public function offsetGet($id)
 	{
-		if (!isset($this->services[$offset])) {
-			throw new FontoException("No service is registered with name $offset");
+		if (!isset($this->services[$id])) {
+			throw new FontoException("No service is registered with name $id");
 		}
 
-		if (is_callable($this->services[$offset])) {
-			return $this->services[$offset]($this);
-		} else {
-			return $this->services[$offset];
-		}
+		return is_callable($this->services[$id]) ? $this->services[$id]($this) : $this->services[$id];
+	}
+
+	/**
+	 * Registers a callback so it is shared.
+	 *
+	 * @param  Closure $callback
+	 * @return Closure
+	 */
+	public function shared(Closure $callback)
+	{
+		return function ($c) use ($callback) {
+			static $object;
+
+			if (null === $object) {
+				$object = $callback($c);
+			}
+
+			return $object;
+		};
 	}
 }
