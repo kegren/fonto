45cef3c9984ee20f08d539056d8991dd28fabce7
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-12 10:51:24 +0100

    Updated validation message and added error checking

--- vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateRequire.php
+++ vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateRequire.php
@@ -1,20 +1,32 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core\Validation\Components;
 
 use Fonto\Core\Validation\Validator;
+use Fonto\Core\Application\App;
 
-class Require extends Validator
+class ValidateRequire extends Validator
 {
-	protected $message = '';
-
-	public function __construct($id, $value, $validateValue)
+	public function __construct(App $app, $value, $validateValue)
 	{
-		$this->validateAttribute($id, $value, $validateValue);
+        $this->message = $app->getConfig()->load('Validation', 'required');
+		$this->validateAttribute($value, $validateValue);
 	}
 
-	protected function validateAttribute($id, $value, $validateValue)
+	protected function validateAttribute($value, $validateValue)
 	{
-		!is_numeric($value) and static::$errors[$id] = array('require' => $this->message);
+		return strlen($value) == 0 and $this->error = $this->message;
 	}
+
+    public function getErrorMessage()
+    {
+        return !empty($this->error) ? $this->error : false;
+    }
 }
