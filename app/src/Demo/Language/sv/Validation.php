fa81e40a7c0c1bda4a71b0affebdddfae0a6e95c
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-12 13:11:28 +0100

    Updated messages

--- app/src/Demo/Language/sv/Validation.php
+++ app/src/Demo/Language/sv/Validation.php
@@ -6,8 +6,8 @@
  */
 
 return array(
-	'max' => 'Värdet är för stort.',
-	'min' => 'Värdet är för litet.',
-	'num' => 'Värdet måste vara ett tal.',
+	'max'      => 'Värdet är för stort.',
+	'min'      => 'Värdet är för litet.',
+	'num'      => 'Värdet måste vara ett tal.',
 	'required' => 'Ett värde krävs.'
 );
