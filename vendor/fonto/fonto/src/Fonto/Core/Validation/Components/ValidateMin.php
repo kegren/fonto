8ec6671a7dc31b6ea4b688056c8667a3ef291d8d
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-12 10:51:07 +0100

    Updated validation message and added error checking

--- vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateMin.php
+++ vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateMin.php
@@ -1,20 +1,32 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core\Validation\Components;
 
 use Fonto\Core\Validation\Validator;
+use Fonto\Core\Application\App;
 
 class ValidateMin extends Validator
 {
-	protected $message = 'Värdet är för litet';
-
-	public function __construct($id, $value, $validateValue)
+	public function __construct(App $app, $value, $validateValue)
 	{
-		$this->validateAttribute($id, $value, $validateValue);
+        $this->message = $app->getConfig()->load('Validation', 'min');
+		$this->validateAttribute($value, $validateValue);
 	}
 
-	protected function validateAttribute($id, $value, $validateValue)
+	protected function validateAttribute($value, $validateValue)
 	{
-		return (strlen($value) < $validateValue) and static::$errors[$id]['min'] =  $this->message;
+		return strlen($value) < $validateValue and $this->error = $this->message;
 	}
+
+    public function getErrorMessage()
+    {
+        return !empty($this->error) ? $this->error : false;
+    }
 }
