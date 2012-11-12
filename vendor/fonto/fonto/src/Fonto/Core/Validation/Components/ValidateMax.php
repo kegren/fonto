d8dde1cb92aa5e5a510e270ecc63abe42c3195e3
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-12 10:50:58 +0100

    Updated validation message and added error checking

--- vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateMax.php
+++ vendor/fonto/fonto/src/Fonto/Core/Validation/Components/ValidateMax.php
@@ -1,20 +1,32 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core\Validation\Components;
 
 use Fonto\Core\Validation\Validator;
+use Fonto\Core\Application\App;
 
-class Max extends Validator
+class ValidateMax extends Validator
 {
-	protected $message = 'Värdet är för stort';
-
-	public function __construct($id, $value, $validateValue)
+	public function __construct(App $app, $value, $validateValue)
 	{
-		$this->validateMax($id, $value, $validateValue);
+        $this->message = $app->getConfig()->load('Validation', 'max');
+		$this->validateAttribute($value, $validateValue);
 	}
 
-	protected function validateMax($id, $value, $validateValue)
+	protected function validateAttribute($value, $validateValue)
 	{
-		(strlen($value) > $validateValue) and static::$errors[$id] = array('max' => $this->message);
+		return strlen($value) > $validateValue and $this->error = $this->message;
 	}
+
+    public function getErrorMessage()
+    {
+        return !empty($this->error) ? $this->error : false;
+    }
 }
