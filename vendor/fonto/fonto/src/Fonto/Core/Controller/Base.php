41a6c5cd1d6789db37fdea6759c0dc87a1eb4d10
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-12 13:12:53 +0100

    Changed name to Base and updated docs

--- vendor/fonto/fonto/src/Fonto/Core/Controller/Base.php
+++ vendor/fonto/fonto/src/Fonto/Core/Controller/Base.php
@@ -7,25 +7,39 @@
  * @link https://github.com/kenren/fonto
  */
 
-namespace Fonto\Core;
+namespace Fonto\Core\Controller;
 
 use Fonto\Core\View;
 use Fonto\Core\Application\App;
 
-class Controller
+class Base
 {
+	/**
+	 * Fonto\Core\Application\App
+	 *
+	 * @var object
+	 */
 	protected $app;
 
 	public function __construct()
-	{
+	{}
 
-	}
-
+	/**
+	 * Sets the current application
+	 *
+	 * @param App $app
+	 */
 	public function setApp(App $app)
 	{
 		$this->app = $app;
 	}
 
+	/**
+	 * Magic method
+	 *
+	 * @param $class
+	 * @param $args
+	 */
 	public function __call($class, $args)
 	{
 		return $this->app->container[$class];
