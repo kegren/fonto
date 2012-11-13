7cd6022abc85efc07151b3c5ce65a6e041a95928
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-13 14:08:29 +0100

    Updated docs

--- app/src/Demo/Models/Forms/Example.php
+++ app/src/Demo/Models/Forms/Example.php
@@ -7,17 +7,23 @@ use Fonto\Core\Validation\Validator;
 
 class Example extends Base
 {
+	/**
+	 * Sets rules for 'Example form'
+	 *
+	 * @param  Validator $validator
+	 * @return Closure
+	 */
 	public function rules(Validator $validator)
 	{
 		$rules = function() use ($validator) {
 			$validator->field('username')
-					   ->max(4)
-					   ->num()
-					   ->required();
+					  ->max(32)
+                      ->min(2)
+					  ->required();
 
 			$validator->field('password')
-					   ->max(3)
-					   ->required();
+					  ->max(32)
+					  ->required();
 		};
 
 		return $rules();
