86039baa85b2c15bc8e60eaccb791ed93fa63c1c
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-14 10:58:19 +0100

    Added new auth class

---
+++ vendor/fonto/fonto/src/Fonto/Core/Authentication/Auth.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
+
+namespace Fonto\Core\Authentication;
+
+class Auth
+{
+
+}
