e363a3f207a6423e75d431428fe6a9bb1e9d9eab
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-14 11:00:00 +0100

    Added authenticate method

--- vendor/fonto/fonto/src/Fonto/Core/Authentication/Auth.php
+++ vendor/fonto/fonto/src/Fonto/Core/Authentication/Auth.php
@@ -11,5 +11,31 @@ namespace Fonto\Core\Authentication;
 
 class Auth
 {
+	private $user;
+	private $app;
+
+	public function __construct()
+	{}
+
+	public function authenticate($username, $password)
+	{
+		$user = new User();
+		$user = User::find_by_username('fonto');
+
+		if ($user) {
+
+			$this->user = $user;
+
+			if ($this->validatePassword('admin')) {
+
+				$this->login($this->user);
+
+				return true;
+			}
+
+		}
+
+		return false;
+	}
 
 }
