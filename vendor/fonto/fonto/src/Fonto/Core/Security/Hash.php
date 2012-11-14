3e9ef212479e17456ac16a4849492c075a2fb1ba
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-14 21:18:20 +0100

    Updated docs

--- vendor/fonto/fonto/src/Fonto/Core/Security/Hash.php
+++ vendor/fonto/fonto/src/Fonto/Core/Security/Hash.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Fonto Framework
+ *
+ * @author Kenny Damgren <kenny.damgren@gmail.com>
+ * @package Fonto
+ * @link https://github.com/kenren/fonto
+ */
 
 namespace Fonto\Core\Security;
 
