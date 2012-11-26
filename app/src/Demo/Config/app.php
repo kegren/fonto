8525fe00470fa4975315244611e32f5cc8340a7f
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-26 21:27:58 +0100

    Added options for language, config and session

--- app/src/Demo/Config/app.php
+++ app/src/Demo/Config/app.php
@@ -5,45 +5,52 @@
  * Application settings
  */
 
- return array(
- 	/**
- 	 * Sets Default timezone
- 	 */
- 	'timezone' => 'Europe/Stockholm',
-
- 	/**
- 	 * Sets database settings
- 	 */
- 	'database' => array(
- 		'development' => array(
-	 		'type' => 'mysql',
-	 		'host' => 'localhost',
-	 		'user' => 'root',
-	 		'pass' => '',
-	 		'name' => 'fontomvc'
- 		),
- 		'production' => array(
-	 		'type' => 'mysql',
-	 		'host' => 'localhost',
-	 		'user' => 'root',
-	 		'pass' => '',
-	 		'name' => 'fonto'
- 		),
- 	),
-
- 	/**
- 	 * Sets application environment, development enables error_reporting(-1)
- 	 */
- 	'environment' => 'development',
-
- 	/**
- 	 * Sets baseUrl for application
- 	 */
- 	'baseUrl' => '',
-
- 	/**
- 	 * Enables twig
- 	 */
- 	'twig' => false,
-
- );
+return array(
+    /**
+     * Sets language
+     */
+    'language' => 'sv',
+    /**
+     * Sets Default timezone
+     */
+    'timezone' => 'Europe/Stockholm',
+    /**
+     * Sets database settings
+     */
+    'database' => array(
+        'local' => array(
+            'type' => 'mysql',
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => '',
+            'name' => 'fontomvc'
+        ),
+        'server' => array(
+            'type' => 'mysql',
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => '',
+            'name' => 'fonto'
+        ),
+    ),
+    /**
+     * Sets application environment, development enables error_reporting(-1)
+     */
+    'environment' => 'local',
+    /**
+     * Sets baseUrl for application
+     */
+    'baseUrl' => '',
+    /**
+     * Enables twig
+     */
+    'twig' => false,
+    /**
+     *
+     */
+    'sessionName' => 'FontoMVC',
+    /**
+     *
+     */
+    'config' => '.php'
+);
