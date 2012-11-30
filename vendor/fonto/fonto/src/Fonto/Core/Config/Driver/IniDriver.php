3d0cb29cd54b7de02006d01086b18ace64b626b0
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-30 08:38:14 +0000

    Added config driver classes

---
+++ vendor/fonto/fonto/src/Fonto/Core/Config/Driver/IniDriver.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
+
+namespace Fonto\Core\Config\Driver;
+
+use Fonto\Core\Config\DriverInterface;
+use Fonto\Core\Config\Base;
+
+class IniDriver extends Base implements DriverInterface
+{
+
+}
