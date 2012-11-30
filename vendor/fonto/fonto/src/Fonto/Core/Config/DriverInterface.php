c102ff63d1453e97d031228e0e7354b24ce8ce8d
Kenny Damgren <kenny.damgren@gmail.com>
2012-11-30 07:45:25 +0000

    Added new config driver interface

---
+++ vendor/fonto/fonto/src/Fonto/Core/Config/DriverInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
+
+interface ConfigInterface
+{
+    public function load(array $options = array());
+}
