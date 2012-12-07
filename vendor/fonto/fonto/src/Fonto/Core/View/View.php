fe86b5a9bea8ac95f462d0eac0a5af3e6db5439b
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-07 20:27:02 +0100

    Changed view behavior and added new driver object

--- vendor/fonto/fonto/src/Fonto/Core/View/View.php
+++ vendor/fonto/fonto/src/Fonto/Core/View/View.php
@@ -9,199 +9,33 @@
 
 namespace Fonto\Core\View;
 
-use Fonto\Core\FontoException;
-use Fonto\Core\Application\App;
+use Fonto\Core\View\Driver\DriverInterface;
 
 class View
 {
-	/**
-	 * File extension
-	 *
-	 * @var string
-	 */
-	private $extension;
+    /**
+     * @var Driver\DriverInterface
+     */
+    protected $driver;
 
-	/**
-	 * Containing output data for the view
-	 *
-	 * @var array
-	 */
-	private $data = array();
-
-	/**
-	 * Viewfile
-	 *
-	 * @var string
-	 */
-	private $view;
-
-	/**
-	 * Fonto\Core\Application\App
-	 *
-	 * @var object
-	 */
-	protected $app;
-
-	/**
-	 * Holds external data
-	 *
-	 * @var array
-	 */
-	private $storage;
-
-
-	public function __construct()
-	{}
-
-	/**
-	 * Sets current application
-	 *
-	 * @param App $app
-	 */
-	public function setApp(App $app)
+    /**
+     * Constructor
+     *
+     * @param Driver\DriverInterface $driver
+     */
+    public function __construct(DriverInterface $driver)
 	{
-		$this->app = $app;
+        $this->driver = $driver;
+    }
 
-		return $this;
-	}
-
-	/**
-	 * Loads external file inside view
-	 *
-	 * @param  string $layout
-	 * @return mixed
-	 */
-	public function load($external)
-	{
-		if (file_exists(VIEWPATH . DS . $external . EXT)) {
-
-			$path = VIEWPATH . DS . $external . EXT;
-
-			echo $this->show($path, $this->storage, true);
-
-			return true;
-		}
-
-		throw new FontoException("The requested view '$external' does not exists");
-	}
-
-	/**
-	 * Sets data for view
-	 *
-	 * @param array $data
-	 */
-	public function setData(array $data = array())
-	{
-		$this->data = $data;
-
-		return $this;
-	}
-
-	/**
-	 * Returns data for the view
-	 *
-	 * @return array
-	 */
-	public function getData()
-	{
-		return $this->data;
-	}
-
-	/**
-	 * Renders a view
-	 *
-	 * @param  string $view Viewfile
-	 * @param  array  $data Data
-	 */
-	public function render($view, $data = null)
-	{
-		$twig = $this->app->getConfig()->load('app', 'twig');
-
-		if ($twig) {
-			$this->setExtension('twig');
-			$this->view = $view . $this->getExtension();
-
-			if (null === $data) {
-				echo $this->app->getTwig()->render($this->view, $this->getData());
-			} else {
-				echo $this->app->getTwig()->render($this->view, $data);
-			}
-		} else {
-            $this->setExtension('php');
-
-            echo $this->show($view, $data);
-        }
-	}
-
-	protected function css($file)
-	{
-		return $this->app->getCss()->getCssFile($file);
-	}
-
-	protected function purify($file)
-	{
-		return $this->app->getPurifier()->purify($file);
-	}
-
-	/**
-	 * Sets up a view for native php
-	 *
-	 * @param  string $view Viewfile
-	 * @param  array  $data Data
-	 * @return mixed
-	 */
-	private function show($view, $data = null, $external = false)
-	{
-	    null === $data and $data = $this->data and $this->storage = $this->data;
-
-	    $this->storage = $data;
-
-        ob_start() and extract($data, EXTR_OVERWRITE);
-
-        try {
-        	if ($external) {
-        		require $view;
-        	} else {
-        		require VIEWPATH . $view . $this->getExtension();
-        	}
-        }
-        catch (\FontoException $e) {
-            ob_end_clean();
-            throw $e;
-        }
-
-        return ob_get_clean();
-	}
-
-	/**
-	 * Sets extension for the view file
-	 *
-	 * @param string $type
-	 */
-	private function setExtension($type)
-	{
-		switch ($type) {
-			case 'twig':
-				$this->extension = '.html.twig';
-				break;
-
-            case 'php':
-                $this->extension = '.php';
-                break;
-
-			default:
-				$this->extension = '.html';
-				break;
-		}
-	}
-
-	/**
-	 * Returns current file extension
-	 *
-	 * @return string
-	 */
-	private function getExtension()
-	{
-		return $this->extension;
-	}
+    /**
+     * Renders a view
+     *
+     * @param $view
+     * @param $data
+     */
+    public function render($view, $data)
+    {
+        echo $this->driver->render($view, $data);
+    }
 }
