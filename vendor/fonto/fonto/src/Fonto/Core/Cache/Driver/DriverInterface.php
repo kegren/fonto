2d25a6decd516019a367ef65d6345fa32d1211e1
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-07 21:06:53 +0100

    Updated docs

--- vendor/fonto/fonto/src/Fonto/Core/Cache/Driver/DriverInterface.php
+++ vendor/fonto/fonto/src/Fonto/Core/Cache/Driver/DriverInterface.php
@@ -8,11 +8,40 @@
  * @version     0.5
  */
 
-namespace Fonto\Core\Cache;
+namespace Fonto\Core\Cache\Driver;
 
 interface DriverInterface
 {
+    /**
+     * Stores a value in the cache
+     *
+     * @param $key
+     * @param $value
+     * @param int $expire
+     * @return mixed
+     */
     public function set($key, $value, $expire = 0);
 
+    /**
+     * Returns a value from the cache
+     *
+     * @param $key
+     * @return mixed
+     */
     public function get($key);
+
+    /**
+     * Deletes a value from the cache
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function delete($key);
+
+    /**
+     * Deletes all content in the cache
+     *
+     * @return mixed
+     */
+    public function flush();
 }
