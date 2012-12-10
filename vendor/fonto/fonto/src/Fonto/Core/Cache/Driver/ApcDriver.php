661c04ac4d428cd76561241a4e98446e732f2960
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-11 00:21:36 +0100

    Updated package name

--- vendor/fonto/fonto/src/Fonto/Core/Cache/Driver/ApcDriver.php
+++ vendor/fonto/fonto/src/Fonto/Core/Cache/Driver/ApcDriver.php
@@ -3,7 +3,7 @@
  * Fonto - PHP framework
  *
  * @author      Kenny Damgren <kenny.damgren@gmail.com>
- * @package     Fonto
+ * @package     Fonto.Core
  * @link        https://github.com/kenren/fonto
  * @version     0.5
  */
