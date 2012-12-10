a19d79856b86998ed34a9cce1c19bf44065f5930
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-10 23:31:44 +0100

    Fixed wrong view argument and updated docs

--- vendor/fonto/fonto/src/Fonto/Core/DependencyInjection/Dependencies.php
+++ vendor/fonto/fonto/src/Fonto/Core/DependencyInjection/Dependencies.php
@@ -1,25 +1,54 @@
 <?php
-
-namespace Fonto\Core\DependencyInjection;
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto.Core
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
 
 return array(
+    /**
+     * Configuration class
+     */
     'Config' => array(
         'class' => '\Fonto\Core\Config\ConfigManager',
+        'id' => 'Config',
         'args' => array(
             'PhpDriver' => '\Fonto\Core\Config\Driver\PhpDriver'
         )
     ),
+    /**
+     * Router class
+     */
     'Router' => array(
         'class' => '\Fonto\Core\Routing\Router',
+        'id' => 'Config',
         'args' => array(
             'Route' => '\Fonto\Core\Routing\Route',
             'Request' => '\Fonto\Core\Http\Request'
         )
     ),
+    /**
+     * Response class
+     */
     'Response' => array(
         'class' => '\Fonto\Core\Http\Response',
+        'id' => 'Response',
         'args' => array(
-            'Url' => '\Fonto\Core\Url'
+            'Url' => '\Fonto\Core\Http\Url',
+            'View' => '\Fonto\Core\View\View'
+        )
+    ),
+    /**
+     * View class
+     */
+    'View' => array(
+        'class' => '\Fonto\Core\View\View',
+        'id' => 'View',
+        'args' => array(
+            'Native' => '\Fonto\Core\View\Driver\Native'
         )
     )
 );
