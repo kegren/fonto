bbf0946067ad1ba058b23fb8e871417735d0b31d
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-10 23:14:28 +0100

    Updated docs

--- vendor/fonto/fonto/src/Fonto/Core/View/Driver/DriverInterface.php
+++ vendor/fonto/fonto/src/Fonto/Core/View/Driver/DriverInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto.Core
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
 
 namespace Fonto\Core\View\Driver;
 
