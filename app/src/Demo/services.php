43a1f4a7d6302d87cf32222b91a5d1f5b8d59178
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-11 00:16:57 +0100

    Added both doctrine and purifier as services

--- app/src/Demo/services.php
+++ app/src/Demo/services.php
@@ -1,9 +1,67 @@
 <?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
 
 use Hautelook\Phpass\PasswordHash;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\ORM\Configuration;
+use Fonto\Core\Config\ConfigManager;
+use Fonto\Core\Config\Driver\PhpDriver;
+
+/**
+ * Configuration object
+ */
+$config = new ConfigManager(new PhpDriver());
 
 $di->setService(
-    'phpass',
+    'EntityManager',
+    function () use ($config) {
+        // TODO: APC
+        $cache = new ArrayCache();
+
+        $env = $config->read('app#environment');
+        $database = $config->read('app#database');
+        $databaseConfiguration = $database[$env];
+
+        $configuration = new Configuration();
+        $configuration->setMetadataCacheImpl($cache);
+        $configuration->setQueryCacheImpl($cache);
+
+        $driverImpl = $configuration->newDefaultAnnotationDriver('model');
+        $configuration->setMetadataDriverImpl($driverImpl);
+        $configuration->setProxyDir('proxies');
+        $configuration->setProxyNamespace('proxies');
+
+        $configuration->setAutoGenerateProxyClasses(true);
+        $configuration->getAutoGenerateProxyClasses();
+
+        $em = EntityManager::create($databaseConfiguration, $configuration);
+
+        return $em;
+    }
+);
+
+$di->setService(
+    'Purifier',
+    function () {
+        $cfg = \HTMLPurifier_Config::createDefault();
+        $purifier = new \HTMLPurifier($cfg);
+
+        return $purifier;
+    }
+);
+
+
+$di->setService(
+    'Phpass',
     function () {
         return new PasswordHash(8, false);
     }
