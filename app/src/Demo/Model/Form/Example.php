5173a848dfd900369833cc6bb5addf9936852e0d
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-30 13:47:16 +0100

    Updated docs

--- app/src/Demo/Model/Form/Example.php
+++ app/src/Demo/Model/Form/Example.php
@@ -1,12 +1,27 @@
 <?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @link     https://github.com/kenren/fonto
+ * @version  0.5
+ */
 
 namespace Demo\Model\Form;
 
-use Fonto\Core\FormModel\Base;
+use Fonto\FormModel\Base;
 
+/**
+ * A basic example how a form model can look like.
+ *
+ * @link    https://github.com/kenren/fonto
+ * @author  Kenny Damgren <kenny.damgren@gmail.com>
+ */
 class Example extends Base
 {
     /**
+     * Sets rules for example form
+     *
      * @return array
      */
     public function rules()
