4e8c11b3249eefe37f94750d36cca9140bc29cd8
Kenny Damgren <kenny.damgren@gmail.com>
2012-12-30 11:31:37 +0100

    Added new html helper class

---
+++ vendor/fonto/fonto/src/Fonto/View/Helper/Html.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author      Kenny Damgren <kenny.damgren@gmail.com>
+ * @package     Fonto_View
+ * @subpackage  Helper
+ * @link        https://github.com/kenren/fonto
+ * @version     0.5
+ */
+
+namespace Fonto\View\Helper;
+
+
+/**
+ * A small helper class for html elements
+ *
+ * @package    Fonto_View
+ * @subpackage Helper
+ * @link       https://github.com/kenren/fonto
+ * @author     Kenny Damgren <kenny.damgren@gmail.com>
+ */
+class Html
+{
+    /**
+     * Returns a html link
+     *
+     * @param string  $baseUrl
+     * @param array   $args
+     * @param string  $text
+     * @return string
+     */
+    public function createLink($baseUrl, $args = array(), $text)
+    {
+        $arguments = '';
+
+        foreach($args as $arg) {
+            $arguments .= $arg . "/";
+        }
+
+        $arguments = substr($arguments, 0, -1);
+        $arguments = $baseUrl.$arguments;
+
+        return '<a href="'.$arguments.'">'.$text.'</a>'."\n";
+    }
+}
