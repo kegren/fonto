210cc23cde9a4af1abaf14ef68bf15197f2fe176
Kenny Damgren <kennydamgren@gmail.com>
2013-07-26 21:39:24 +0200

    Remove ACTIVE_APP reference and remove setPaths method

--- vendor/fonto/fonto/src/Fonto/Application/App.php
+++ vendor/fonto/fonto/src/Fonto/Application/App.php
@@ -37,7 +37,6 @@ class App extends ObjectHandler
      */
     public function __construct()
     {
-        $this->setPaths();
         parent::__construct();
     }
 
@@ -50,7 +49,7 @@ class App extends ObjectHandler
     public function run($loader)
     {
         try {
-            $loader->add(ACTIVE_APP, APPPATH . 'src');
+            $loader->add("Demo", APPPATH . 'modules');
 
             $config = $this->config();
             $this->setTimezone($config->read('app#timezone'));
@@ -81,41 +80,5 @@ class App extends ObjectHandler
     protected function setTimezone($timezone)
     {
         date_default_timezone_set($timezone);
-    }
-
-    /**
-     * Defines paths based on the application name
-     *
-     * @return void
-     */
-    private function setPaths()
-    {
-        if (!defined('CONFIGPATH')) {
-            define('CONFIGPATH', APPPATH . 'src' . DS . ACTIVE_APP . DS . 'Config' . DS);
-        }
-
-
-        if (!defined('APPWEBPATH')) {
-            define('APPWEBPATH', APPPATH . 'src' . DS . ACTIVE_APP . DS);
-        }
-
-        if (!defined('CTLPATH')) {
-            define('CTLPATH', APPPATH . 'src' . DS . ACTIVE_APP . DS . 'Controller' . DS);
-        }
-
-
-        if (!defined('VIEWPATH')) {
-            define('VIEWPATH', APPPATH . 'src' . DS . ACTIVE_APP . DS . 'View' . DS);
-        }
-
-        if (!defined('SESSPATH')) {
-            define('SESSPATH', APPWEBPATH . 'Storage' . DS . 'Session' . DS);
-        }
-
-
-        if (!defined('MODELPATH')) {
-            define('MODELPATH', APPPATH . 'src' . DS . ACTIVE_APP . DS . 'Model' . DS);
-        }
-
     }
 }
