ea8201f46a0da92f3e019b70e9e3798572af1022
Kenny Damgren <kennydamgren@gmail.com>
2013-07-28 11:33:18 +0200

    Add new facade class

---
+++ vendor/fonto/fonto/src/Fonto/Application/Facade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_Application
+ * @link     https://github.com/kenren/fonto
+ * @version  0.5
+ */
+
+namespace Fonto\Application;
+
+class Facade
+{
+
+}
