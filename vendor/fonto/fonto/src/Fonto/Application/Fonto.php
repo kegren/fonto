ee913b6db6b1d6731a9e67ebfa1dc8812b21b4e6
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 00:34:54 +0200

    Add new base factory class

---
+++ vendor/fonto/fonto/src/Fonto/Application/Fonto.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_Application
+ * @link     https://github.com/kenren/fonto
+ * @version  0.6
+ */
+
+namespace Fonto\Application;
+
+use Fonto\DependencyInjection\ServiceLocator;
+
+class Fonto
+{
+    public function grab($class)
+    {
+        if ($obj = $this->getServiceLocator()->get($class)) {
+            return $obj;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns a service locator instance. Used for creating a new
+     * instance.
+     *
+     * @return [type] [description]
+     */
+    public function getServiceLocator()
+    {
+        return new ServiceLocator();
+    }
+}
