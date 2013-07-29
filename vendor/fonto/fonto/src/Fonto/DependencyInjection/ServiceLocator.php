cf11d8aae80b797b5ae28c553cf8574305290772
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 00:35:15 +0200

    Add new service locator class

---
+++ vendor/fonto/fonto/src/Fonto/DependencyInjection/ServiceLocator.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_DependencyInjection
+ * @link     https://github.com/kenren/fonto
+ * @version  0.6
+ */
+
+namespace Fonto\DependencyInjection;
+
+use Fonto\DependencyInjection\ServiceBuilder;
+use Fonto\Application\Facade;
+
+class ServiceLocator extends ServiceBuilder
+{
+    protected $services = array();
+
+    /**
+     * Returns an object based on service name.
+     *
+     * @param  string $service
+     * @return mixed
+     */
+    public function get($service)
+    {
+        $service = strtolower($service);
+
+        if (isset($this->services[$service])) {
+            return new $service();
+        }
+
+        if ($core = $this->getCore($service)) {
+            return $this->resolveObject($core);
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if a given class is registered as a core service.
+     * Returns array containing information about the class.
+     *
+     * @param  string $service
+     * @return mixed
+     */
+    public function getCore($service)
+    {
+        $core = $this->getCoreServiceFile();
+
+        if (array_key_exists($service, $core)) {
+            return $core[$service];
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns core services for Fonto.
+     *
+     * @return array
+     */
+    public function getCoreServiceFile()
+    {
+        return include __DIR__ . '/services.php';
+    }
+}
