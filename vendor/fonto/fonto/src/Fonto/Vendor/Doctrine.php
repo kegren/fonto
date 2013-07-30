90f40ee3af1a99a760c0b7f7be47c20e210cb6a2
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 04:18:08 +0200

    Adding new doctrine class

---
+++ vendor/fonto/fonto/src/Fonto/Vendor/Doctrine.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_Vendor
+ * @link     https://github.com/kegren/fonto
+ * @version  0.6
+ */
+
+namespace Fonto\Vendor;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\ORM\Configuration;
+use Doctrine\Common\Cache\ApcCache;
+use Fonto\Facade\Config;
+
+class Doctrine
+{
+
+    public function connect()
+    {
+        $cfg = Config::grab('app', true);
+
+        if ($cfg['cache'] == 'apc') {
+            $cache = new ApcCache();
+        } else {
+            $cache = new ArrayCache();
+        }
+
+        $env = $cfg['env'];
+        $db = $cfg['db'];
+
+        $settings = $db[$env];
+
+        $configuration = new Configuration();
+        $configuration->setMetadataCacheImpl($cache);
+        $configuration->setQueryCacheImpl($cache);
+
+        $driverImpl = $configuration->newDefaultAnnotationDriver('model');
+        $configuration->setMetadataDriverImpl($driverImpl);
+        $configuration->setProxyDir(STORAGEPATH . 'proxies');
+        $configuration->setProxyNamespace('proxies');
+
+        $configuration->setAutoGenerateProxyClasses(true);
+        $configuration->getAutoGenerateProxyClasses();
+
+        return EntityManager::create($settings, $configuration);
+    }
+
+}
