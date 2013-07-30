f80cc3bee0e384de69d2c47d94432f2827cbcafc
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 04:16:25 +0200

    Add new config class

---
+++ vendor/fonto/fonto/src/Fonto/Config/Config.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_Config
+ * @link     https://github.com/kegren/fonto
+ * @version  0.6
+ */
+
+namespace Fonto\Config;
+
+class Config
+{
+    /**
+     * Configuration file
+     *
+     * @var string
+     */
+    protected $file = null;
+
+    /**
+     * Returns a file if found else the nullable above.
+     *
+     * @param  string  $file
+     * @param  boolean $return
+     * @return Fonto\Config
+     */
+    public function grab($file, $return = false)
+    {
+        if ($file = $this->getFile($file)) {
+            $this->file = $file;
+        }
+
+        if ($return) {
+            return $this->file;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns a specific key in the array.
+     *
+     * @param  string $option
+     * @return mixed
+     */
+    public function get($option)
+    {
+        if ($this->file) {
+            return isset($this->file[$option]) ? $this->file[$option] : null;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns a file if found else false
+     *
+     * @param  string $file
+     * @return mixed
+     */
+    protected function getFile($file)
+    {
+        $file = CONFIGPATH . '/' . $file . '.php';
+
+        return file_exists($file) ? include $file : false;
+    }
+
+}
