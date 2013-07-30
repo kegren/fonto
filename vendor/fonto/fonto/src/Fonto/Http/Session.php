86b1189c41d3ac688787eeffca8a1bbfcb7bf254
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 04:32:14 +0200

    Update docs

--- vendor/fonto/fonto/src/Fonto/Http/Session.php
+++ vendor/fonto/fonto/src/Fonto/Http/Session.php
@@ -4,8 +4,8 @@
  *
  * @author   Kenny Damgren <kenny.damgren@gmail.com>
  * @package  Fonto_Http
- * @link     https://github.com/kenren/fonto
- * @version  0.5
+ * @link     https://github.com/kegren/fonto
+ * @version  0.6
  */
 
 namespace Fonto\Http;
@@ -14,7 +14,7 @@ namespace Fonto\Http;
  * A wrapper for PHP sessions.
  *
  * @package Fonto_Http
- * @link    https://github.com/kenren/fonto
+ * @link    https://github.com/kegren/fonto
  * @author  Kenny Damgren <kenny.damgren@gmail.com>
  */
 class Session
@@ -26,7 +26,7 @@ class Session
 	 */
 	public function __construct($sessionName = null)
 	{
-        $this->setSessionSavePath(SESSPATH);
+        $this->setSessionSavePath(STORAGEPATH . '/session');
         if (!$this->isStarted()) {
             $this->setName($sessionName);
             $this->start();
