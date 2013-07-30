3006bbeef14cd0bded7fbc909e20ac8f39c3cd81
Kenny Damgren <kennydamgren@gmail.com>
2013-07-30 04:17:30 +0200

    Add new facade classes

---
+++ vendor/fonto/fonto/src/Fonto/Facade/Url.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Fonto - PHP framework
+ *
+ * @author   Kenny Damgren <kenny.damgren@gmail.com>
+ * @package  Fonto_Facade
+ * @link     https://github.com/kegren/fonto
+ * @version  0.6
+ */
+
+namespace Fonto\Facade;
+
+use Fonto\Facade\Facade;
+
+class Url extends Facade
+{
+    public static function getFacade()
+    {
+        return 'Url';
+    }
+}
